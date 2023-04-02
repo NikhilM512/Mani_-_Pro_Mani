1b107401a280b0122d12d6160574c1ccf3dd7688
akashkanade9182 <akashkanade235@gmail.com>
2023-04-02 14:37:22 +0530

    aboute page addded

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Homepage from './Pages/Homepage';
 import Navbar from './Component/Navbar';
+import AllRoutes from './Pages/AllRoutes';
 function App() {
   return (
     <>
     <Navbar/>
-    <Homepage/>
+    <AllRoutes/>
     </>
   );
 }
